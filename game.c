6998ea83f733d2bb6b7ca08c4876783564a41ff6
Morales Arugula <marugula@at-d5.msk.21-school.ru>
2022-06-23 14:21:13 +0300

    go to work Nasta

--- game.c
+++ game.c
@@ -15,7 +15,7 @@ void	paint_sample(t_data *data)
 	}
 }
 
-float	find_intersection_point(t_data *data)
+float	find_intersection_point_axis(t_data *data)
 {
 	// float	step_x;
 	float	step_y;
@@ -28,19 +28,59 @@ float	find_intersection_point(t_data *data)
 	return (0);
 }
 
+t_vector	find_intersection_points(t_data *data, float angle_ray)
+{
+	t_vector	axis_point;
+	t_vector	ordinat_point;
+	t_vector	axis_step;
+	t_vector	ordinat_step;
+
+	if (sin(angle_ray) > 0)
+	{
+		axis_point.y = floor(data->pl.poz.y / 64) * 64;
+		axis_step.y = -64;
+	}
+	else
+	{
+		axis_point.y = ceil(data->pl.poz.y / 64) * 64;
+		axis_step.y = 64;
+	}
+	axis_step.x = axis_step.y / tan(angle_ray);
+
+	if (cos(angle_ray) > 0)
+	{
+		ordinat_point.x = ceil(data->pl.poz.x / 64) * 64;
+		ordinat_step.x = 64
+	}
+	else
+	{
+
+	}
+
+
+
+
+	return ((float) 0)
+}
+
 
 
 
 void	ray_cast(t_data *data)
 {
 	float	angle_ray;
+	t_vector	intersection_poin;
 	float	distance;
 	// вычесление крайнего левого луча
 	angle_ray = data->pl.direction - (FOV / 2);
 
-	while (angle_ray < data->pl.direction)
+	while (angle_ray < data->pl.direction + (FOV / 2))
 	{
-		distance =
+		intersection_poin = find_intersection_points(data, angle_ray)
+	// расстояние
+	// считаем высоту
+	// получаем номер колонки
+	// ОТРИСОВКА
 
 		angle_ray += STEPANGLE;
 	}
@@ -52,14 +92,16 @@ void	ray_cast(t_data *data)
 
 void game(char **map, t_textures textures)
 {
-	t_data			data;
+	t_data	data;
 
 	data.window.mlx = mlx_init();
 	data.window.win = mlx_new_window(data.window.mlx, WIDTH, HEIGHT, "Cub3D");
 	creat_window_img(&data.window);
 	init_sides_img(&data.imgs, textures, data.window.mlx);
 	data.map = map;
+	// init pl poz
 
+	// ray_cast();
 
 	fill_floor_and_cell_window_img(&data.window.img, textures);
 	paint_sample(&data);
