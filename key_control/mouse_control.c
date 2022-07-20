031b7d674a863e3226cfb628a2f60e1730bd1394
Thanh Amchoor <tamchoor@at-j3.msk.21-school.ru>
2022-07-20 12:26:07 +0300

    norm change

--- key_control/mouse_control.c
+++ key_control/mouse_control.c
@@ -6,7 +6,7 @@
 /*   By: tamchoor <tamchoor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 15:51:10 by tamchoor          #+#    #+#             */
-/*   Updated: 2022/07/20 12:20:28 by tamchoor         ###   ########.fr       */
+/*   Updated: 2022/07/20 12:25:09 by tamchoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	mouse_move(int x, int y, t_data *data)
 	(void) y;
 	if (data->key.press_x != x)
 	{
-		data->pl.direction -= \
-					(x - data->key.press_x) * (float)(M_PI / 2) / 90 * SPEED_MOUSE_MOVE;
+		data->pl.direction -= (x - data->key.press_x) * \
+					(float)(M_PI / 2) / 90 * SPEED_MOUSE_MOVE;
 		data->key.press_x = x;
 		data->key.mouse_move = 1;
 	}
--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marugula <marugula@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tamchoor <tamchoor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 13:28:48 by tamchoor          #+#    #+#             */
-/*   Updated: 2022/07/14 20:25:35 by marugula         ###   ########.fr       */
+/*   Updated: 2022/07/20 12:25:43 by tamchoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	count;
 
 	count = 0;
-	while (s &&
-	s[count] != 0)
+	while (s && s[count] != 0)
 	{
 		count++;
 	}
